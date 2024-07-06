224adaa0e84505da4d286207c2ca206267982381
Yezhiqi-cc <1208382819@qq.com>
2024-07-06 17:46:36 +0800

    Add files via upload

---
+++ _html/code/index.js
@@ -0,0 +1,14 @@
+var audio = document.getElementById("不眠之夜");
+var img = document.getElementById("yyan");
+function toggleAudio() {
+    if (audio.paused) {
+      audio.play();
+      img.src = "../resource/99.png"
+      document.getElementById("不眠之夜").innerHTML = "暂停";
+    } else {
+      audio.pause();
+      img.src = "../resource/666.png"
+      document.getElementById("不眠之夜").innerHTML = "播放";
+    }
+  }
+  
