1f9b047a4ee363b3c7e0eb502e6eb44841ee379f
Yezhiqi-cc <1208382819@qq.com>
2024-07-06 18:38:38 +0800

    Update index.js

--- _html/code/index.js
+++ _html/code/index.js
@@ -3,11 +3,11 @@ var img = document.getElementById("yyan");
 function toggleAudio() {
     if (audio.paused) {
       audio.play();
-      img.src = "../resource/99.png"
+      img.src = "./_html/resource/99.png"
       document.getElementById("不眠之夜").innerHTML = "暂停";
     } else {
       audio.pause();
-      img.src = "../resource/666.png"
+      img.src = "./_html/resource/666.png"
       document.getElementById("不眠之夜").innerHTML = "播放";
     }
   }
