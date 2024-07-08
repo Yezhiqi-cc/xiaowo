e2102b4d3fa30d9087024a8e0452e2c5bda53d6d
Yezhiqi-cc <1208382819@qq.com>
2024-07-08 17:28:01 +0800

    Update index.js

--- index.js
+++ index.js
@@ -65,12 +65,14 @@
             }
         })(window, document);
 
-
+//打开b站
 function openb(){
+    var op = confirm("是否跳转到B站个人空间?")
+    if(op == true){
     window.open("https://space.bilibili.com/353184926?spm_id_from=333.1007.0.0");
   }
 
-
+}
 let mucics = document.getElementById('音乐');
 document.body.addEventListener('mousemove', function() {
         setTimeout(() => {
@@ -81,7 +83,7 @@ document.body.addEventListener('mousemove', function() {
 var music = document.getElementById('music')    //获取音乐
 var musicico = document.getElementById('musicico')   //获取音乐图标
 var tem = true  //设置一个变量，用来控制音乐是否在播放。
-
+document.getElementById("music").volume = 0.5;//设置音量大小
 //定义一个函数，当用户单击的时候触发这个函数，从而实现音乐的暂停与播放。
 function musiccc(){
 	//tem用来控制音乐当前是否在播放。true代表音乐正在播放，false代表音乐当前正在处于暂停的状态。
@@ -95,3 +97,23 @@ function musiccc(){
 		document.getElementById('musicico').style.animationPlayState = 'paused'  //暂停音乐图标
 	}
 }
+document.getElementById('toggleButton').addEventListener('click',  
+    function() {  
+        // 获取ID为'content'的元素，通常是一个包含文本或其他内容的容器  
+        var content = document.getElementById('c1');  
+      
+        // 检查'content'元素的maxHeight样式属性是否存在值  
+        if (content.style.maxHeight) {  
+            // 如果maxHeight存在值，说明内容当前是被限制的，我们将其设为null来展开全部内容  
+            content.style.maxHeight = null; // 展开全部内容    
+      
+            // 同时将按钮的文本改为'展开阅读全文'，提示用户可以点击收起内容  
+            this.textContent = '展开阅读全文'; // 按钮文本改为“收起全文”    
+        } else {  
+            // 如果maxHeight不存在值，说明内容当前是全部展开的，我们将其设为内容的scrollHeight来收起内容到限定高度  
+            content.style.maxHeight = content.scrollHeight + 'px'; // 收起内容到限定高度    
+      
+            // 同时将按钮的文本改为'收起'，提示用户可以点击展开全部内容  
+            this.textContent = '收起'; // 按钮文本改为“查看全文”    
+        }  
+    });
